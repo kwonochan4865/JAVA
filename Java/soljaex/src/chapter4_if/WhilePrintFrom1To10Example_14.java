c24887ffb79ff8565c5597651a1d7a776d59ef6c
ochan <pmnlug1@naver.com>
2020-04-30 11:33:32 +0900

    chap5

---
+++ Java/soljaex/src/chapter4_if/WhilePrintFrom1To10Example_14.java
@@ -0,0 +1,13 @@
+package chapter4_if;
+
+public class WhilePrintFrom1To10Example_14 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while (i<=10) {
+			System.out.println(i);
+			
+			i++;
+		}
+	}
+}
