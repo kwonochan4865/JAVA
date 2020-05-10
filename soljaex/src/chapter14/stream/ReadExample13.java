1b1d1352d49375f4c9b1570da93e199434f4f979
ochan <pmnlug1@naver.com>
2020-05-10 16:20:18 +0900

    데스크탑 변경 사항

--- soljaex/src/chapter14/stream/ReadExample13.java
+++ soljaex/src/chapter14/stream/ReadExample13.java
@@ -9,7 +9,7 @@ public class ReadExample13 {
 
 		char[] buffer = new char[5];
 
-		int readCharNum = reader.read(buffer, 2, 3, 4 ); // 입력 스트림으로부터 3개의 문자를 읽고 buffer[2], buffer[3], buffer[4]에 각각 저장
+		int readCharNum = reader.read(buffer, 2, 3); // 입력 스트림으로부터 3개의 문자를 읽고 buffer[2], buffer[3], buffer[4]에 각각 저장
 		if (readCharNum != -1) {
 			for (int i = 0; i < buffer.length; i++) {
 				System.out.println(buffer[i]);
